40e86669b8f16fa8c3d23cf3420f222cd00d59cb
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-12 10:02:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-class Area():
+class Circle():
     def __init__(self, radius):
         self.radius = radius
 
-    def calc_hekef(self,radius):
+    def calc_hekef(self):
         return  (self.radius)*3.14*2
 
-    def calc_area(self,radius):
+    def calc_area(self):
         return (self.radius)*3.14**2
 
     def __str__(self):
-        return f'the radius is {radius}, the hekef is {self.calc_hekef} and the area is {self.area}'
-
-radius = Area(6)
-print(radius,radius.calc_hekef(radius),radius.calc_hekef(radius))
+        return f'the radius is {radius1}' \
+               f'the hekef is {self.calc_hekef()}' \
+               f'and the area is {self.calc_area()}'
